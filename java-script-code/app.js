9273229cca2cfd7bfc840ffb4a480a5de55e9ec4
Your Name <you@example.com>
2023-07-19 16:58:54 +0530

    com

---
+++ java-script-code/app.js
@@ -0,0 +1,11 @@
+let atext= " dharshini priya";
+let len= atext.length;
+console.log("the string length is "+len);
+let half= atext.slice(-3);
+let hal= atext.slice(3);
+let ha= atext.slice(12);
+console.log(half);
+console.log(hal);
+console.log(ha);
+let rep=atext.replace('dharshini','krishna');
+console.log(rep);
