31e6610263af73d990cbdd6997c5997d911bdf81
Your Name <you@example.com>
2023-07-20 14:26:22 +0530

    commit

---
+++ java-script-code/arrowfunc.js
@@ -0,0 +1,15 @@
+ getName =()=>{
+    console.log("hiii guys");
+}
+getName();
+console.log(getName);
+//arrow function will behave like a variable 
+//only when called gets executed
+//this is hoisting only proper function declaration gets the whole functions
+// hosting means calling a function before it gets declared 
+var x=10;
+a();
+function a(){
+    var x=1;
+    console.log(x);
+}
