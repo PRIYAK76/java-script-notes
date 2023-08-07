748b418e7ef806aece32bbd28d1c6a0302279659
Your Name <you@example.com>
2023-08-07 15:41:32 +0530

    ccc

---
+++ java-script-code/json.js
@@ -0,0 +1,33 @@
+let emp={}
+emp.name="priya";
+emp.id=1;
+emp.email="priya@gmail.com";
+emp.address={}
+emp.address.street=12;
+emp.address.city="Palladam";
+emp.address.District="Tiruppur";
+console.log(emp.address);
+
+
+/*"id":1,
+    "name":"Priyadharshini K",
+    "username":"Bret",
+    "email":"priya@gmail.com",
+    "address":{
+        "street":12,
+        "city":"Palladam",
+        "District":"Tiruppur",
+        "Pin code":"641664",
+        "geo":{
+            "lat":"-37.78654",
+            "Ing":"81.35665"
+        }
+    },
+    "mobile":["mi","apple","samsung"],
+    "phone": 8015967442,
+    "Website":"priya.com",
+    "company":{
+        "name":"Romaguera-Crona",
+        "catchPhrase": "Multi-layered client-server neural-net",
+        "bs": "harness real-time e-markets"  
+    }*/
